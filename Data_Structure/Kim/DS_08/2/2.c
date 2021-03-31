8a70e8898b5bfd1a11a6f527ea9be8169ff5eb1b
Jihoon Nam <mpq05@naver.com>
2021-03-31 22:34:47 +0900

    update 210331

--- Data_Structure/Kim/DS_08/2/2.c
+++ Data_Structure/Kim/DS_08/2/2.c
@@ -1,71 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MALLOC(p, s) \
-	if( !( (p) = malloc( s ) ) ){	\
-		fprintf(stderr, "Insufficient memory");\
-		exit(EXIT_FAILURE);\
+	if( !((p) = malloc(s))) { \
+		fprintf(stderr, "Insufficient memory"); \
+		exit( EXIT_FAILURE );\
 	}
 
-// linked list
 typedef struct listNode {
 	int data;
-	struct listNode * link;
+	struct listNode* link;
 }listNode;
-listNode *first = NULL;
+listNode* first = NULL;
 
-void find(listNode *first, listNode **x, int data);				// find insert position
-void insert(listNode **first, listNode *x, int data);				// insert data into ordered list first after node x
-void delete(listNode **first, listNode *trail, listNode *x); // 주의 : delete 는 c++ 의 keyword!, 소스파일 확장자가 cpp이면 컴파일 에러임
-void printList(listNode *first);
-
-int main(void)
+void printList(listNode* first); // list를 출력해주는 함수
+void insert(listNode** first, listNode* x, int data_f);
+void delete(listNode** first, listNode* trail, listNode* x);
+void locate(listNode* first, listNode** x, int data_f); // 정렬을 위해 위치를 잡아줄 함수
+int main()
 {
-	int data;
-	listNode *x, *trail, *curr, *ffirst;
+	FILE* fp;
+	int data_f; // 파일에서 값을 정수를 받아올 변수
+	listNode* x, * trail;
+	listNode* p1, * p2;
 
-	// data input for each node
-	FILE *fp;
-	fopen_s(&fp, "input.txt", "r");
-	if (fp == NULL)
-	{
-		fprintf(stderr, "cannot open the file");
-		exit(EXIT_FAILURE);
-	}
+	if (fopen_s(&fp, "input.txt", "r"))
+		exit(1);
 
-	fscanf_s(fp, "%d", &data);
-	while (!feof(fp))
-	{
-		find(first, &x, data);		// find insert position
-		insert(&first, x, data);		// insert data first after node x. 
-		fscanf_s(fp, "%d", &data);
+	fscanf_s(fp, "%d", &data_f);
+	// 데이터를 받고 정렬해서 계속 연결
+	while (!feof(fp)) {
+		locate(first, &x, data_f);
+		insert(&first, x, data_f);
+		fscanf_s(fp, "%d", &data_f);
 	}
+	// 홀수 삭제를 위해 listPointer를 준비
+	trail = NULL;
+	p1 = first;
+	p2 = first;
+
+	printf("The ordered list contains:\n");
 	printList(first);
 
-	curr = ffirst = first;
-	trail = NULL;
-
-	while (curr != NULL)
+	while (p1 != NULL)
 	{
-		if (curr->data % 2) {
-			x = curr;
+		if (p1->data % 2 != 0) {
+			x = p1;
 			delete(&first, trail, x);
-			if (ffirst != first) {
+			if (p2 != first) {
 				trail = NULL;
-				curr = first;
-				ffirst = first;
+				p1 = first;
+				p2 = first;
 			}
-			else
-				curr = trail->link;
-
+			else {
+				p1 = trail->link;
+			}
 		}
-		else
-		{
-			trail = curr;
-			curr = curr->link;
+		else {
+			trail = p1;
+			p1 = p1->link;
 		}
 	}
 
-	printf("\nAfter deleting nodes with odd value \n\n");
+	printf("\nAfter deleting nodes with odd value\n\n");
+	printf("The ordered list contains:\n");
 	printList(first);
 
 	fclose(fp);
@@ -73,79 +71,71 @@ int main(void)
 	return 0;
 }
 
-void find(listNode *first, listNode **x, int data)
-{ /* *x is the position of insert  */
-	listNode *pre = first;
-	listNode *trav = first;
+void printList(listNode* first)
+{
+	int i;
 
-	if ( first == NULL )
-	{ // empty list
-		*x = first;
+	for (i = 1; first; first = first->link, i++) {
+		printf("(%p, %4d, %p ) ", first, first->data, first->link);
+		if ((i % 3) == 0)
+			printf("\n");
 	}
-	else 
-	{ // non-empty list
-		for ( ; trav; trav = trav->link)
-		{  	
-			if( data > trav->data)
-			{
-				pre = trav;
-				*x = pre;
-			}
-			else
-			{					
-				if ( first == pre )
-					*x = NULL;
-				break;
-			}
-		}
-	}
+
 }
 
-void insert(listNode **first, listNode *x, int data)
-{ /* insert a new node with a data into the chain first after node x */
-	listNode * temp;
-	MALLOC(temp, sizeof(*temp));
-	temp->data = data;
-
-	if(*first == NULL) 
-	{ // add to empty list
-		temp->link = NULL;
-		*first = temp;
-	}
-	else 
-	{ // add to non-empty list
-		if ( x == NULL ) 
-		{ // as a first node
-			temp->link = *first;
-			*first = temp;
-		}
-		else 
-		{ 
-			temp->link = x->link;
-			x->link = temp;
-		}
-	}
-}
-
-void delete(listNode **first, listNode *trail, listNode *x)
-{/* delete x from the list, trail is the preceding node 
-	   and *first is the front of the list */
-	if(trail)
+void delete(listNode** first, listNode* trail, listNode* x)
+{
+	if (trail)
 		trail->link = x->link;
 	else
 		*first = (*first)->link;
 	free(x);
 }
 
-void printList(listNode *first)
+void insert(listNode** first, listNode* x, int data_f)
 {
-	int count;
-	printf("The ordered list constains: \n");
-	for (count = 1; first; first = first->link, count++)
-	{
-		printf("(%p, %4d, %p )%s", first, first->data, first->link, count % 3 ? "" : "\n");
-		if (count % 10 == 0)
-			printf("\n");
+	listNode* temp;
+	MALLOC(temp, sizeof(*temp));
+	temp->data = data_f;
+	if (*first == NULL) {
+		temp->link = NULL;
+		*first = temp;
 	}
-	printf("\n");
+	else {
+		if (x == NULL) {
+			temp->link = *first;
+			*first = temp;
+		}
+		else {
+			temp->link = x->link;
+			x->link = temp;
+		}
+	}
+}
+
+void locate(listNode* first, listNode** x, int data_f)
+{
+	listNode* p1 = first;
+	listNode* p2 = first;
+
+	if (first == NULL) {
+		*x = first;
+	}
+	else
+	{
+		for (; p2; p2 = p2->link)
+		{
+			if (data_f > p2->data)
+			{
+				p1 = p2;
+				*x = p1;
+			}
+			else
+			{
+				if (first == p1)
+					*x = NULL;
+				break;
+			}
+		}
+	}
 }
