d5d0f3fd04ddb6aa64e091e101dc9eb832255405
Jihoon Nam <mpq05@naver.com>
2021-03-31 22:35:15 +0900

    update 210331

--- Data_Structure/Kim/DS_08/3/3.c
+++ Data_Structure/Kim/DS_08/3/3.c
@@ -1,134 +1,120 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 20
 #define MALLOC(p, s) \
-	if( !( (p) = malloc( s ) ) ){	\
-		fprintf(stderr, "Insufficient memory");\
-		exit(EXIT_FAILURE);\
+	if( !((p) = malloc(s))) { \
+		fprintf(stderr, "Insufficient memory"); \
+		exit( EXIT_FAILURE );\
 	}
 
-// linked list
-typedef struct listNode *listPointer;
+typedef struct listNode* listPointer;
 typedef struct listNode {
-	char name[10];
-	char job[20];
-	char gen[5];
+	char name[MAX_LEN];
+	char be[MAX_LEN];
+	char sex[MAX_LEN];
 	listPointer link;
 }listNode;
 listPointer first = NULL;
 
-// void find(listPointer first, listPointer *x, listNode data);					// find insert position
-void insert(listPointer *first, listPointer *x, listNode data);				// insert data into ordered list first after node x
-// void delete( listPointer *first, listPointer trail, listPointer x); // 주의 : delete 는 c++ 의 keyword!, 소스파일 확장자가 cpp이면 컴파일 에러임
-void printList(listPointer first);
+void printList(listPointer first); // list를 출력해주는 함수
+void insert(listPointer* first, listPointer* x, listNode data_f);
 
-int main(void)
+int main()
 {
-	listNode data_n;
-
-
-	listPointer trail, cur, man, woman, man_e, woman_e;
-	
-	// data input for each node
-	FILE *fp;	
-
-	fopen_s(&fp, "input.txt", "r");
-	if( fp == NULL )
-	{
-		fprintf(stderr, "cannot open the file");
-		exit(EXIT_FAILURE);
-	}
-	
+	FILE* fp;
+	listNode data_f; // 파일에서 값을 받아올 구조체
+	listPointer trail, man, woman;
+	listPointer p1, temp1, temp2;
+	man = NULL;
+	woman = NULL;
+	temp1 = NULL;
+	temp2 = NULL;
 	trail = NULL;
-	fscanf_s(fp, "%s%s%s", data_n.name, 10, data_n.job, 20, data_n.gen, 5);		
-	while( !feof(fp) )
-	{
-//		find(first, &x, data);		// find insert position
-		insert(&first, &trail , data_n);		// insert data first after node x. 
-		fscanf_s(fp, "%s%s%s", data_n.name, 10, data_n.job, 20, data_n.gen, 5);
+
+	if (fopen_s(&fp, "input.txt", "r"))
+		exit(1);
+
+	fscanf_s(fp, "%s%s%s", data_f.name, MAX_LEN, data_f.be, MAX_LEN, data_f.sex, MAX_LEN);
+	// 데이터를 받고 연결
+	while (!feof(fp)) {
+		insert(&first, &trail, data_f);
+		fscanf_s(fp, "%s%s%s", data_f.name, MAX_LEN, data_f.be, MAX_LEN, data_f.sex, MAX_LEN);
 	}
-	insert(&first, &trail, data_n);		// insert data first after node x. 
-	printf("전체 리스트 \n");
-	printList(first);	
+	insert(&first, &trail, data_f);
+	p1 = first;
 
-	man_e = woman_e = NULL;
-	cur = first;
-	man = woman = NULL;
-
-	while (cur) {
-		if (!strcmp(cur->gen, "남자")) {
-			if (man)
-				man_e->link = cur;				
-			else
-				man = cur;
-			man_e = cur;
+	printf("전체 리스트\n");
+	printList(first);
+	
+	while (p1 != NULL)
+	{
+		if (strcmp(p1->sex, "남자") == 0) {
+			if (man) {
+				temp1->link = p1;
+			}
+			else {
+				man = p1;
+			}
+			temp1 = p1;
 		}
 		else {
 			if (woman)
-				woman_e->link = cur;
-			else
-				woman = cur;
-			woman_e = cur;
+				temp2->link = p1;
+			else {
+				woman = p1;
+			}
+			temp2 = p1;
 		}
-		cur = cur->link;
+		p1 = p1->link;
 	}
-	man_e->link = NULL;
-	woman_e->link = NULL;
+	temp1->link = NULL;
+	temp2->link = NULL;
 
-	printf("남자 리스트 \n");
+	printf("\n남자 리스트\n");
 	printList(man);
-	printf("\n여자 리스트 \n");
+	printf("\n\n여자 리스트\n");
 	printList(woman);
 	
 	fclose(fp);
-	
+
 	return 0;
 }
 
+void printList(listPointer first)
+{
+	int i;
 
-void insert(listPointer *first, listPointer *x, listNode n_data)
-{ /* insert a new node with a data into the chain first after node x */
+	for (i = 1; first; first = first->link, i++) {
+		printf("(%p, %s, %s, %s, %p )  ", first, first->name, first->be, first->sex, first->link);
+		if ((i % 2) == 0)
+			printf("\n");
+	}
+}
+
+void insert(listPointer* first, listPointer *x, listNode data_f)
+{
 	listPointer temp;
 	MALLOC(temp, sizeof(*temp));
-	strcpy_s(temp->name, 10, n_data.name);
-	strcpy_s(temp->job, 20, n_data.job);
-	strcpy_s(temp->gen, 5, n_data.gen);
+	strcpy_s(temp->name, MAX_LEN, data_f.name);
+	strcpy_s(temp->be, MAX_LEN, data_f.be);
+	strcpy_s(temp->sex, MAX_LEN, data_f.sex);
 
-	if(*first == NULL)  
-	{ // add to empty list
+	if (*first == NULL) {
 		temp->link = NULL;
 		*first = temp;
 		*x = temp;
 	}
-	else 
-	{ // add to non-empty list
-			
-		if ( x == NULL ) 
-		{ // as a first node
+	else {
+		if (x == NULL) {
 			temp->link = *first;
 			*first = temp;
 			*x = temp;
 		}
-		else 
-		{ 
+		else {
 			temp->link = (*x)->link;
 			(*x)->link = temp;
 			*x = temp;
 		}
-/*		
-		temp->link = x->link;
-		x->link = temp;
-*/
-
 	}
-}
-
-
-void printList(listPointer listp)
-{
-	int count;
-//	printf("The ordered list contains: \n");
-	for (count = 1 ; listp; listp = listp->link, count++)
-		printf("(%p, %s, %s, %s %p )   %s", listp, listp->name, listp->job, listp->gen, listp->link, count%2 ? "" : "\n");
-	printf("\n");
 }
