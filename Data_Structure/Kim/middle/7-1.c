893544778958bc8e8ad17607f4b46bb8d35abb52
Jihoon Nam <mpq05@naver.com>
2021-04-21 12:33:10 +0900

    210421 study

--- Data_Structure/Kim/middle/7-1.c
+++ Data_Structure/Kim/middle/7-1.c
@@ -6,36 +6,58 @@
 #define MAX_EXPR_SIZE 100
 #define MAX_LENGTH 80
 
-typedef enum {lparen, rparen, plus, minus, times, divide, mod, eos, operand}precedence;
+typedef enum { lparen, rparen, plus, minus, times, divide, mod, eos, operand }precedence;
 
-int stack[MAX_STACK_SIZE];
-char expr[MAX_EXPR_SIZE];
+int stack[MAX_STACK_SIZE]; // 과정을 저장할 stack
+char expr[MAX_EXPR_SIZE]; // 연산자를 저장할 배열
 int top = -1;
 
+// stack 의 push , pop 함수
+void push(int item);
+int pop();
+// postfix 관련 함수
+precedence getToken(char* symbol, int* n); // 연산자, 피연산자 구분 및 토큰 분리
 int eval(char* symbol);
 
-int main(void)
-{	
+
+// main 함수
+int main()
+{
 	FILE* fp1, * fp2;
 	char Token[MAX_LENGTH];
 	int result;
 
-	fopen_s(&fp1, "input.txt", "r");
-	fopen_s(&fp2, "output.txt", "w");
-
-	if (fp1 == NULL || fp2 == NULL)
+	if (fopen_s(&fp1, "input.txt", "r"))
+		exit(1);
+	if (fopen_s(&fp2, "output.txt", "w"))
 		exit(1);
 
-	fgets(Token, MAX_LENGTH, fp1);
-	result = eval(Token);
+	fgets(Token, sizeof(Token), fp1); // input에서 입력 받아오기
+	result = eval(Token); // 함수 사용
+	printf("postfix expression : %s\n", Token);
+	printf("the evaluation value : %d", result);
+
+	fprintf(fp2, "postfix expression : %s\n", Token);
+	fprintf(fp2, "the evaluation value : %d", result);
+
+	fclose(fp1);
+	fclose(fp2);
 
 	return 0;
 }
 
+
+// functions 
 void push(int item) {
 	if (top >= MAX_STACK_SIZE - 1)
-		printf("stack is full");
+		printf("stack is full\n");
 	else stack[++top] = item;
+}
+
+int pop() {
+	if (top == -1)
+		printf("stack is empty\n");
+	return stack[top--];
 }
 
 precedence getToken(char* symbol, int* n) // 연산자, 피연산자 구분 및 토큰 분리
